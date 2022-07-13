aa79bcaa6b60c9fcc1fd7ab0e1096cc6f5c82bb0
Barbara Hartmann <49559279+BabsBerlin@users.noreply.github.com>
2022-07-13 13:38:55 +0200

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Blueberry Oatmeal')
+streamlit.text('🥣 Blueberry Oatmeal')
 streamlit.text('Scrambled Eggs')
+streamlit.text('Green Smoothie')
+streamlit.text('🥑 Avocado Toast')
