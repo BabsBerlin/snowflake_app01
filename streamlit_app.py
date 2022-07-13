d4bfd6f17195b67fa7b1f5fcb937b5615a02b88a
Barbara Hartmann <49559279+BabsBerlin@users.noreply.github.com>
2022-07-13 13:44:23 +0200

    Update streamlit_app.py
    imported fruit list dataframe from aws bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import streamlit
+
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -9,3 +11,6 @@ streamlit.text('🥗 Green Salad')
 streamlit.text('🥑 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
