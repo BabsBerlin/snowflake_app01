cf41c5b3b056b23e055685993027ec2548f87f5a
Barbara Hartmann <49559279+BabsBerlin@users.noreply.github.com>
2022-07-13 13:40:24 +0200

    Update streamlit_app.py
    and some more content

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('🥣 Blueberry Oatmeal')
-streamlit.text('Scrambled Eggs')
-streamlit.text('Green Smoothie')
+streamlit.text('🐔 Scrambled Eggs')
+streamlit.text('🥗 Green Salad')
 streamlit.text('🥑 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
