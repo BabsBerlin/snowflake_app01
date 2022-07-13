abc9b566ab52deb6f11995d759158658942fc892
Barbara Hartmann <49559279+BabsBerlin@users.noreply.github.com>
2022-07-13 13:51:42 +0200

    Update streamlit_app.py
    fixed error set_index

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list.set_index('Fruit')
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -15,6 +16,6 @@ streamlit.text('🥑 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # add pick list
-streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.set_index('Fruit')))
+streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index))
 # display fruit table
 streamlit.dataframe(my_fruit_list)
