cbdb36856f2018c476c3732afde190e8966c069a
Barbara Hartmann <49559279+BabsBerlin@users.noreply.github.com>
2022-07-14 13:04:41 +0200

    Update streamlit_app.py
    added fruityvice api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import requests
 import streamlit
 
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -20,3 +22,8 @@ fruits_selected = streamlit.multiselect('Pick some fruits: ', list(my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display fruit table
 streamlit.dataframe(fruits_to_show)
+
+# new section to show the api response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.header('Fruityvice Fruit Advice!')
+streamlit.text(fruityvice_response.json())
